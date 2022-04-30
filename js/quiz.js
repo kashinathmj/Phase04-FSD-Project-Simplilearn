448a42cd2e8a12e7adabfdb8cbbddca24bd40cc6
kashinathmj <kashinath.ece@gmail.com>
2022-04-30 16:25:06 +0530

    Phase4 Project Commit

---
+++ js/quiz.js
@@ -0,0 +1,55 @@
+let quiz=[
+    {
+        question:"Which of the following class in Bootstrap is used to provide a responsive fixed width container?",
+        option:[
+            "1) HTML describes the structure of a webpage",
+            "2) HTML is the standard markup language mainly used to create web pages",
+            "3) HTML consists of a set of elements that helps the browser how to view the content",
+            "4) All of the mentioned",
+        ],
+        answer:4,
+    },
+    {
+        question:"Which of the following is not a HTML5 tag?",
+        option:[
+            "1) <track>",
+            "2) <video>",
+            "3) <slider>",
+            "4) <source>",
+            
+        ],
+        answer:3,
+    },
+    {
+        question:"Which attribute is not essential under <iframe>?",
+        option:[
+            "1. frameborder",
+            "2. width",
+            "3. height",
+            "4. src",   
+        ],
+        answer:1,
+    },
+    {
+        question:"Which process model should be used in virtually all situations of web engineering?",
+        option:[
+            "1. Incremental Model",
+            "2. Waterfall Model",
+            "3. Spiral Model",
+            "4. None of the mentioned",   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"Which of the following statements are incorrect with reference to web-based systems?",
+        option:[
+            "1. should be unscalable",
+            "2. Falsemust be able to cope with uncertain, random heavy demands on services",
+            "3, must be secure",
+            "4. are subject to assorted legal, social, and ethical scrutiny",
+               
+        ],
+        answer:1,
+    }
+]
